64a0981815c9d6beaed5cb7120fc4d41f99ab25b
Daryyya <darya9841402670@gmail.com>
2023-03-17 23:05:01 +0300

    fix: styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Container from "@/components/ui/Container/Container";
 import Layout from "@/components/layout/Layout";
-import Button from "@/components/ui/Button/Button";
 import s from "@/assets/styles/Home.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import CardLink from "@/components/ui/CardLink/CardLink";
 import Card from "@/components/ui/Card/Card";
+import download from "@/assets/images/download.svg";
 
 export default function Home() {
   return (
@@ -13,11 +13,22 @@ export default function Home() {
       <Container>
         <Card className={s.mainBlock}>
           <div>
-            <p>все предложения</p>
-            <h1>Корпоративные системы</h1>
+            <p className={s.subtitle}>все предложения</p>
+            <h1 className={s.title}>Корпоративные системы</h1>
           </div>
-          <Link href="assets/files/test.pdf" download="test.pdf" target="_blank">
-            Скачать бриф
+          <Link
+            className={s.link}
+            href="assets/files/test.pdf"
+            download="test.pdf"
+            target="_blank"
+          >
+            <span className={s.link__text}>
+              <span className={s.link__subtitle}>Создание проекта</span>
+              <span>Скачать бриф</span>
+            </span>
+            <span className={s.link__img_wrap}>
+              <Image src={download} alt="скачать" width={30} height={22} />
+            </span>
           </Link>
         </Card>
         <CardLink
